0f5e5d2d9658f71df347aba394eb2dac38c0135e
Jóni Batista <batista.joni@gmail.com>
2020-03-23 11:26:44 +0000

    Adds manager role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
-    before_action :authorize, :set_user
+    before_action :set_user, :authorize
 
     protected
       def authorize
-        unless User.find_by(id: session[:user_id])
+        unless @current_user
           redirect_to login_url, notice: 'Please login first'
+          return
+        end
+
+        if @current_user.reporter? && manager_page?
+          pp "BANG!!!"
+          redirect_to tasks_url, notice: "You don't have permissions to visit that page"
         end
       end
 
@@ -15,5 +21,9 @@ class ApplicationController < ActionController::Base
         @current_user = User.find(session[:user_id])
       rescue ActiveRecord::RecordNotFound
         @current_user = nil
+      end
+
+      def manager_page?
+        params[:controller] != "tasks" && params[:controller] != "sessions"
       end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,11 @@ class User < ApplicationRecord
 
     before_save :clear_email
 
+
+    def reporter?
+      !manager?
+    end
+
     private
       def clear_email
         self.email = email.downcase
