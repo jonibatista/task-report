14ce52e0360f64fa33dc477d7d4eb87bf3ac9af1
Jóni Batista <batista.joni@gmail.com>
2021-01-08 13:58:09 +0000

    Configures mail server to delivery emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'task-report.herokuapp.com', protocol: 'https' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'mail.tecmic.pt',
+    port:                 587,
+    domain:               'tecmic.pt',
+    user_name:            Rails.application.credentials.mail[:username],
+    password:             Rails.application.credentials.mail[:password],
+    authentication:       'plain',
+    enable_starttls_auto: true }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
