a2189b6e0404f326d21f106f49a707646c73a84b
Jóni Batista <batista.joni@gmail.com>
2021-01-10 23:25:35 +0000

    Review mailers' receipts

--- app/mailers/task_report_mailer.rb
+++ app/mailers/task_report_mailer.rb
@@ -8,12 +8,12 @@ class TaskReportMailer < ApplicationMailer
   def rejected(task_report)
     @task_report = task_report
     @title = "#{task_report.user.name}'s #{task_report.title} week report was rejected" 
-    mail(from: 'joni.batista@tecmic.pt', to: task_report.user.email, cc: 'antonio.marcelo@tecmic.pt', subject: @title)
+    mail(from: 'joni.batista@tecmic.pt', to: task_report.user.email, subject: @title)
   end
 
   def approved(task_report)
     @task_report = task_report
     @title = "#{task_report.user.name}'s #{task_report.title} week report was approved" 
-    mail(from: 'joni.batista@tecmic.pt', to: task_report.user.email, cc: 'antonio.marcelo@tecmic.pt', subject: @title)
+    mail(from: 'joni.batista@tecmic.pt', to: [task_report.user.email, 'antonio.marcelo@tecmic.pt'], cc: 'suporte.fsetp@tecmic.pt', subject: @title)
   end
 end
