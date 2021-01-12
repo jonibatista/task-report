1cd7bc34db5a40898706b6e836ae09c958f6e57d
Jóni Batista <batista.joni@gmail.com>
2021-01-12 10:00:24 +0000

    Sets mail server account as default sender

--- app/mailers/task_report_mailer.rb
+++ app/mailers/task_report_mailer.rb
@@ -2,18 +2,18 @@ class TaskReportMailer < ApplicationMailer
   def submitted(task_report)
     @task_report = task_report
     @title = "#{task_report.user.name} submited #{task_report.title} week report for approval" 
-    mail(from: task_report.user.email, to: 'joni.batista@tecmic.pt', cc: 'antonio.marcelo@tecmic.pt', subject: @title)
+    mail(to: 'joni.batista@tecmic.pt', subject: @title)
   end
 
   def rejected(task_report)
     @task_report = task_report
     @title = "#{task_report.user.name}'s #{task_report.title} week report was rejected" 
-    mail(from: 'joni.batista@tecmic.pt', to: task_report.user.email, subject: @title)
+    mail(to: task_report.user.email, subject: @title)
   end
 
   def approved(task_report)
     @task_report = task_report
     @title = "#{task_report.user.name}'s #{task_report.title} week report was approved" 
-    mail(from: 'joni.batista@tecmic.pt', to: [task_report.user.email, 'antonio.marcelo@tecmic.pt'], cc: 'suporte.fsetp@tecmic.pt', subject: @title)
+    mail(to: [task_report.user.email, 'antonio.marcelo@tecmic.pt'], cc: 'suporte.fsetp@tecmic.pt', subject: @title)
   end
 end
