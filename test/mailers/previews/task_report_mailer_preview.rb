7cd7ed26097ca4e0d222f61d1622eb8cc95ceba7
Jóni Batista <batista.joni@gmail.com>
2021-01-18 00:25:24 +0000

    Removes task summary from mail when rejected

--- test/mailers/previews/task_report_mailer_preview.rb
+++ test/mailers/previews/task_report_mailer_preview.rb
@@ -1,14 +1,14 @@
 # Preview all emails at http://localhost:3000/rails/mailers/task_report_mailer
 class TaskReportMailerPreview < ActionMailer::Preview
     def submited
-      TaskReportMailer.submitted(TaskReport.first)
+      TaskReportMailer.submitted(TaskReport.order(:created_at).first)
     end
 
     def rejected
-      TaskReportMailer.rejected(TaskReport.first)
+      TaskReportMailer.rejected(TaskReport.order(:created_at).first)
     end
 
     def approved
-      TaskReportMailer.approved(TaskReport.first)
+      TaskReportMailer.approved(TaskReport.order(:created_at).first)
     end
 end
